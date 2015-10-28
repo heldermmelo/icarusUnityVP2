251ae165035464ecb46135dcc6da82003c7382ff
Helder de Medeiros Melo <heldermmelo@gmail.com>
2015-10-28 12:49:56 -0300

    tubos exportados para fazer montanhas vazadas

--- Assets/VP2/scripts/Menu.cs
+++ Assets/VP2/scripts/Menu.cs
@@ -1,20 +1,23 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 using System;
 
 public class Menu : MonoBehaviour {
 
-    bool estaVisivel = true;
-   
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    bool estaVisivel = false;
+
+    // Use this for initialization
+    void Start () {
+
+       
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-
+     
         if (Input.GetKeyDown(KeyCode.P) && (estaVisivel == false))
         {
             estaVisivel = true;
@@ -23,24 +26,31 @@ public class Menu : MonoBehaviour {
         {
             estaVisivel = false;
 
-
         }
     }
     //a classe que vai desenhar interface gráfica
-    void OnGUI() {
+    Boolean OnGUI() {
         if (estaVisivel)
         {
             if (GUI.Button(new Rect(250, 180, 80, 60), "Novo"))
             {
+                
                 print("Você apertou o botão");
                 Application.LoadLevel(0);
+
+                
             }
             else if (GUI.Button(new Rect(500, 180, 90, 60), "Proximo nível"))
             {
+                
                 print("Você apertou o botão");
                 Application.LoadLevel(1);
-            }
-        }
+
+                
+			}
+            
+		}
+        return estaVisivel;
     }
       
     private Rect newRect(int v1, int v2, int v3, int v4, string v5)
